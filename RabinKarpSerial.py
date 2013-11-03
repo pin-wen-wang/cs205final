95bfe5389865cc4d14d19b7ec1e4884e8407077f
pwwang <pin-wen_wang@college.harvard.edu>
2013-11-03 11:06:55 -0500

    adding command line txt input

--- RabinKarpSerial.py
+++ RabinKarpSerial.py
@@ -2,7 +2,7 @@
 # http://www.geeksforgeeks.org/searching-for-patterns-set-3-rabin-karp-algorithm#/
 # serial Rabin Karp algorithm
 #########################################
-
+import sys
 d = 256 # number of characters in input alphabet?
 
 def search(txt,pat,q):
@@ -13,34 +13,43 @@ def search(txt,pat,q):
   h = 1
 
   for i in range(0,patlen-1):
-	  print "creating h\n"
+	#  print "creating h\n"
 	  h = (h*d)%q
   for i in range(0,patlen):
-	  print "creating hash values\n"
+	#  print "creating hash values\n"
 	  hashpat = (d*hashpat + ord(pat[i]))%q
 	  hashtxt = (d*hashtxt + ord(txt[i]))%q
   
   for i in range(0,txtlen-patlen+1):
-	  print "running through txt\n"
+	#  print "running through txt\n"
 	  # check all the letters if the hashes match
 	  if (hashpat == hashtxt):
-		  print "hashes are equal\n"
+	#	  print "hashes are equal\n"
 		  for j in range (0,patlen):
 			  if (txt[i+j] != pat[j]):
 				break
 		  	  if j == patlen-1:
 			  	print "pattern found at index %d" %i 
+				print "pattern: %s" %txt[i:i+patlen]
 				return
 
 	  if (i < txtlen-patlen):
-		print "shifting pat in txt\n"
+	#	print "shifting pat in txt\n"
 	  	hashtxt = (d*(hashtxt - ord(txt[i])*h) + ord(txt[i+patlen]))%q
 		if (hashtxt < 0):
 			hashtxt = hashtxt + q
 
 if __name__ == '__main__':
 
-  txt = "my name is bob"
-  pat = "name is"
+  datatxt, pattxt = sys.argv[1:] 
+  with open (datatxt, "r") as txtfile:
+	  txt=txtfile.read().replace('\n', '')
+  with open (pattxt,"r") as patfile:
+	  pat=patfile.read().replace('\n','') 
+  txt = txt.upper() 
+  pat = pat.upper()
+  #txt = "my name is bob"
+  #pat = "name is"
   q = 101
+
   search(txt,pat,q)
