08139137bfebe8351f2a48cfe7ec72716db459fa
pwwang <pin-wen_wang@college.harvard.edu>
2013-11-10 16:04:06 -0500

    adding in functions to split input text

--- RabinKarpSerial.py
+++ RabinKarpSerial.py
@@ -3,41 +3,67 @@
 # serial Rabin Karp algorithm
 #########################################
 import sys
-d = 256 # number of characters in input alphabet?
+d = 26 # number of characters in input alphabet?
 
-def search(txt,pat,q):
+def splitCount(s, count):
+    return [''.join(x) for x in zip(*[list(s[z::count]) for z in range(count)])]
+
+def sub_search(txt,pat,q):
+
   patlen = len(pat)
   txtlen = len(txt)
   hashpat = 0
+  hashpat2 = 0
   hashtxt = 0
+  hashtxt2 = 0
   h = 1
+  h2 = 1 
+  tuple_array = []
 
   for i in range(0,patlen-1):
 	#  print "creating h\n"
 	  h = (h*d)%q
+  	  h2 = (h2*d)%q2 
   for i in range(0,patlen):
 	#  print "creating hash values\n"
 	  hashpat = (d*hashpat + ord(pat[i]))%q
+          hashpat2 = (d*hashpat2 + ord(pat[i]))%q2
 	  hashtxt = (d*hashtxt + ord(txt[i]))%q
-  
+          hashtxt2 = (d*hashtxt2 + ord(txt[i]))%q2
+
   for i in range(0,txtlen-patlen+1):
 	#  print "running through txt\n"
 	  # check all the letters if the hashes match
 	  if (hashpat == hashtxt):
 	#	  print "hashes are equal\n"
+	#	  double hashin 
+	#	if(hashpat2==hashtxt2):
+	#		tuple_array.append((i,txt[i:i+patlen]))
+	#		print "pattern found at index %d" %i
+	#		print "pattern %s" %txt[i:i+patlen]
 		  for j in range (0,patlen):
 			  if (txt[i+j] != pat[j]):
 				break
-		  	  if j == patlen-1:
+		 	  if j == patlen-1:
 			  	print "pattern found at index %d" %i 
 				print "pattern: %s" %txt[i:i+patlen]
-				return
+		#		return
 
 	  if (i < txtlen-patlen):
-	#	print "shifting pat in txt\n"
-	  	hashtxt = (d*(hashtxt - ord(txt[i])*h) + ord(txt[i+patlen]))%q
-		if (hashtxt < 0):
-			hashtxt = hashtxt + q
+	#   print "shifting pat in txt\n"
+	    hashtxt = (d*(hashtxt - ord(txt[i])*h) + ord(txt[i+patlen]))%q
+	    hashtxt2 = (d*(hashtxt2 - ord(txt[i])*h2) + ord(txt[i+patlen]))%q2
+	    if (hashtxt < 0):
+		hashtxt = hashtxt + q
+	    if (hashtxt2 < 0):
+		hashtxt2 = hashtxt2 + q2
+
+def full_search(txt,pat,q,patsize):
+  
+  splitpat = splitCount(pat,patsize)
+
+  for subpat in range(0,len(splitpat)):
+	  sub_search(txt,splitpat[subpat],q) 
 
 if __name__ == '__main__':
 
@@ -50,6 +76,8 @@ if __name__ == '__main__':
   pat = pat.upper()
   #txt = "my name is bob"
   #pat = "name is"
-  q = 101
+  q = 1079
+  q2 = 1011
+  patsize = 100;
 
-  search(txt,pat,q)
+  full_search(txt,pat,q,patsize)
