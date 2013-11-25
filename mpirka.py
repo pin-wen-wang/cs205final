0ff682d61afcba0e46b6b8986bb1f1b2a70e3f5f
Pin-Wen Wang <wang.pinwen@gmail.com>
2013-11-24 20:51:07 -0500

    working parallel rka for one file

--- mpirka.py
+++ mpirka.py
@@ -1,6 +1,10 @@
+#
+#usage: python mpirka2.py [single text] [pattern]
+#
 from mpi4py import MPI
 import numpy as np
 import sys
+import string
 
 # set up communication world
 comm = MPI.COMM_WORLD
@@ -13,7 +17,13 @@ d = 26
 def splitCount(s, count):
 	return[s[i:i+count] for i in range(0,len(s),count)]
 
+def prep_text(text):
+	exclude = set(string.punctuation)
+	return ''.join(x.upper() for x in text if x not in exclude)	
+
 def sub_search(txt,pat,q):
+	#print "txt from rank %d: %s" %(rank,txt)
+	#print "pat from rank %d: %s" %(rank,pat)
 	txtlen = len(txt)
 	patlen = len(pat)
 	hashpat = 0
@@ -30,28 +40,39 @@ def sub_search(txt,pat,q):
 		  hashpat = (d*hashpat + ord(pat[i]))%q
 		  hashtxt = (d*hashtxt + ord(txt[i]))%q
 
+	match_list = []
 	for i in range(0,txtlen-patlen+1):
 		#  print "running through txt\n"
 		  # check all the letters if the hashes match
 		  if (hashpat == hashtxt):
-			 comm.send(txt[i:i+patlen],dest=0,tag=i)
+		#	  match_list.append((i,txt[i:i+patlen]))
+	 	# 	  comm.send(txt[i:i+patlen],dest=0,tag=i)
 		#	  print "hashes are equal\n"
 		#	  double hashin
 		#	if(hashpat2==hashtxt2):
-		#		tuple_array.append((i,txt[i:i+patlen]))
+		#		match_list.append((i,txt[i:i+patlen]))
 		#		print "pattern found at index %d" %i
 		#		print "pattern %s" %txt[i:i+patlen]
-			#  for j in range (0,patlen):
-			#	  if (txt[i+j] != pat[j]):
-			#		break
-			 #	  if j == patlen-1:
-			 #	  	comm.send(txt[i:i+patlen],dest=0,tag=i)
+			  for j in range (0,patlen):
+				if (txt[i+j] != pat[j]):
+					break
+			 	if j == patlen-1:
+					match_list.append((i,txt[i:i+patlen]))
 
 		  if (i < txtlen-patlen):
 		#   print "shifting pat in txt\n"
-		    hashtxt = (d*(hashtxt - ord(txt[i])*h) + ord(txt[i+patlen]))%q
-		    if (hashtxt < 0):
-			hashtxt = hashtxt + q
+		  	hashtxt = (d*(hashtxt - ord(txt[i])*h) + ord(txt[i+patlen]))%q
+		  	if (hashtxt < 0):
+				hashtxt = hashtxt + q
+
+ 	comm.send(match_list,dest=0)
+
+def full_search(txt,pat,q,patsize):
+
+	splitpat=splitCount(pat,patsize)
+	
+	for subpat in range(0,len(splitpat)):
+		sub_search(txt,splitpat[subpat],q)
 
 
 # distribute data to other processes to do computations
@@ -59,27 +80,29 @@ searchtxt, pattxt = sys.argv[1:]
 with open (pattxt,"r") as patfile:
 	pat=patfile.read().replace('\n',' ')
 
-pat = pat.upper()
+pat = prep_text(pat) 
+patlen = len(pat)
+#print "patlen %d" %patlen
 
 if rank == 0:
 
-	searchtxt, pattxt = sys.argv[1:]
 	with open (searchtxt,"r") as txt:
 		txt = txt.read().replace('\n',' ')
 		
-	txt = txt.upper()
+	txt = prep_text(txt) 
 
 	txtlen = len(txt)
-	patlen = len(pat)
+	#print "txtlen %d" %txtlen
 
 	local_data = txt[0:int(round(1*(txtlen-patlen+1)/size)+(patlen-1))]
+
 	for i in xrange(1,size):
 		start = int(round((i*(txtlen-patlen+1)/size)))
 		end = int(round((i+1)*(txtlen-patlen+1)/size)+(patlen-1))
-		print start
-		print end
-		if end >= txtlen :
-			end = txtlen-1
+		#print start
+		#print end
+		if end > txtlen :
+			end = txtlen
 		send_data = txt[start:end]
 		#need to keep track of start so we know the absolute index
 		comm.send(send_data,dest=i)
@@ -90,18 +113,22 @@ else :
 q = 1079
 patsize = 100
 
-sub_search(local_data,pat,q)
+full_search(local_data,pat,q,patsize)
+
+#sub_search(local_data,pat,q)
 
 # 0 processes all the results
 if rank == 0:
 
 	status = MPI.Status()
+	
 	for i in xrange(1,size):
 		recv_result = comm.recv(source=i,tag=MPI.ANY_TAG)
 		start = int(round((i*(txtlen-patlen+1)/size)))
-		index = status.Get_tag()
-		abs_index = start+index
-		print "pattern found at index %d" %(abs_index)
-		print "pattern: %s" %recv_result
-		#calculate absolute index
+
+	  	for index, match in recv_result:
+		       	abs_index = start+index
+	       		print "pattern found at index %d" %(abs_index)
+	       		print "pattern: %s" %match
+	       		#calculate absolute index
 
